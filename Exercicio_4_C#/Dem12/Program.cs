b01590c7e41ccbe0e08d8400cc46656c153a31bc
P97arruda <pliniodev97@gmail.com>
2025-12-02 16:43:07 +0000

    Ex 13

---
+++ Exercicio_4_C#/Dem12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Dem12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            Console.WriteLine($"Categoria {(idade < 18 ? "Menor" : (idade <= 64 ? "Adulto" : "Senior"))}");
+        }
+    }
+}
---
+++ Exercicio_4_C#/Dem13/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Dem13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int soma = 0;
+
+            while(contador <= 5)
+            {
+                soma += contador;
+                contador++;
+            }
+
+            Console.WriteLine($"Soma final {soma}");
+        }
+    }
+}
