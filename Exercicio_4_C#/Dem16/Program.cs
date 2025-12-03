51ed3e1405488b5eea45601f707db13f917746a7
P97arruda <pliniodev97@gmail.com>
2025-12-03 13:18:39 +0000

    New push

---
+++ Exercicio_4_C#/Dem16/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Dem16
+{
+    internal class Program
+    {
+        enum diasSemana { Domingo = 1 , Segunda = 2, Terca = 3, Quarta = 4, Quinta = 5, Sexta = 6, Sabado = 7}
+        static void Main(string[] args)
+        {
+
+            int numero;
+            
+            Console.Write("Insita um numero: ");
+            int.TryParse(Console.ReadLine(), out numero);
+
+            diasSemana dias = (diasSemana)numero;
+
+            switch(dias)
+            {
+                case diasSemana.Domingo:
+                Console.WriteLine("Domingo");
+                break;
+
+                case diasSemana.Segunda:
+                    Console.WriteLine("Segunda-feira");
+                    break;
+
+                case diasSemana.Terca:
+                    Console.WriteLine("Terca-feira");
+                    break;
+
+                case diasSemana.Quarta:
+                    Console.WriteLine("Quarta-feira");
+                    break;
+
+                case diasSemana.Quinta:
+                    Console.WriteLine("Quinta-feira");
+                    break;
+
+                case diasSemana.Sexta:
+                    Console.WriteLine("Sexta-feira");
+                    break;
+
+                case diasSemana.Sabado:
+                    Console.WriteLine("Sabado");
+                    break;
+
+                default:
+                    Console.WriteLine("Numero invalido.");
+                    break;
+            }
+           
+        }
+           
+    }
+}
---
+++ Exercicio_4_C#/Dem17/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dem17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
