db838a714842c92353cbac844f49ec31e5f55b7b
p97arruda <p97arruda@gmail.com>
2026-01-02 14:38:19 +0000

    Ver se funciona meu git push

--- ExerciciosPraticos_C#/Entrada de Dados/Ex_1/Program.cs
+++ ExerciciosPraticos_C#/Entrada de Dados/Ex_1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World Plinio!");
+            Console.WriteLine("Vamos co tudo !");
         }
     }
 }
