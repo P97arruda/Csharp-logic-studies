474cb3a5311c3f0caf41afe99fedd4df79cfc78f
p97arruda <p97arruda@gmail.com>
2026-01-03 21:10:27 +0000

    test

--- ExerciciosPraticos_C#/IF-ELSE/Ex_1/Program.cs
+++ ExerciciosPraticos_C#/IF-ELSE/Ex_1/Program.cs
@@ -9,6 +9,8 @@
             Console.WriteLine("Insira um numero: ");
             int.TryParse(Console.ReadLine(), out numero);
 
+            Console.WriteLine($"O numero inserido foi: {numero}");
+
 
 
         }
