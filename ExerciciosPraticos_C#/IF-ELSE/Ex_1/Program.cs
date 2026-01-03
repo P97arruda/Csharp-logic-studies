b8e9d1df8d43d1d68bdbfa3ad7e4dd7d26b35f00
p97arruda <p97arruda@gmail.com>
2026-01-03 21:27:20 +0000

    If Else

--- ExerciciosPraticos_C#/IF-ELSE/Ex_1/Program.cs
+++ ExerciciosPraticos_C#/IF-ELSE/Ex_1/Program.cs
@@ -2,15 +2,29 @@
 {
     internal class Program
     {
+        //Leia um número inteiro e informe se é par ou ímpar.
+        // Ex.: Entrada 4 → Saída Par
+
         static void Main(string[] args)
         {
             int numero; 
 
-            Console.WriteLine("Insira um numero: ");
+            Console.Write("Insira um numero: ");
             int.TryParse(Console.ReadLine(), out numero);
 
-            Console.WriteLine($"O numero inserido foi: {numero}");
 
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero inserido e par.");
+            }
+            else
+            {
+                Console.WriteLine("O numero inserido e impar.");
+
+
+            }
+
+            
 
 
         }
