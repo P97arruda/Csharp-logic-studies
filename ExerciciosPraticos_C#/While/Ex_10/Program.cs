7b863827a11d9cfe488d7558fb34b242f1000049
p97arruda <p97arruda@gmail.com>
2026-01-08 09:52:04 +0000

    Ex_10

---
+++ ExerciciosPraticos_C#/While/Ex_10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            char resposta = 's';
+            
+
+            Console.Write("Insira sua idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            while (resposta == 's')
+            {
+                if (idade < 18)
+                {
+                    Console.WriteLine("Você é menor de idade.");
+                }
+                else
+                {
+                    Console.WriteLine("Você é maior de idade.");
+                }
+                Console.Write("Deseja verificar outra idade? (s/n): ");
+                char.TryParse(Console.ReadLine(), out resposta);
+                if (resposta == 's')
+                {
+                    Console.Write("Insira sua idade: ");
+                    int.TryParse(Console.ReadLine(), out idade);
+                }
+                 
+            }
+        }
+    }
+}
