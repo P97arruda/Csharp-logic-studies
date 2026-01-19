01127dbd27984aa30b1fc1e030afa1f91321a6ea
p97arruda <p97arruda@gmail.com>
2026-01-19 15:26:29 +0000

    Matriz

---
+++ ExercicioMatrizes_7/dem1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace dem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Infermaria = new int[24, 4];
+
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                
+            }
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for(int j = 0; j < 4; j++)
+            //    {
+            //        Console.WriteLine($"Pos[{i},{j}] = {tabela[i,j]}");
+            //    }
+            //}
+
+        }
+    }
+}
