08145c8b4c0123cabf029082893b636ad4f3cf00
p97arruda <p97arruda@gmail.com>
2026-01-20 22:14:44 +0000

    Exercicio C#

---
+++ ExercicioMatrizes_7/Dem2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Dem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[10, 10];
+
+            for (int i = 0; i < 10; i++) 
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    do
+                    {
+
+                        Console.Write($"insira um número [{i + 1}, {j + 1}] : ");
+                    } while(!int.TryParse(Console.ReadLine(), out numeros[i, j]));
+
+                }  
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(numeros[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+
+    }
+}
--- ExercicioMatrizes_7/dem1/Program.cs
+++ ExercicioMatrizes_7/dem1/Program.cs
@@ -1,39 +1,125 @@
-﻿using System.Runtime.Serialization.Formatters;
+﻿
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Transactions;
 
 namespace dem1
 {
     internal class Program
     {
+        /*
+         * Numa enfermaria existem 4 camas (cama 1, cama 2, cama 3 e cama 4) onde se encontram 4
+          pacientes.
+          De hora em hora são medidas as pulsações a cada paciente (ao longo do dia = 24 leituras para
+          cada paciente)
+          Desenvolva um programa que:
+          
+        a) Proceda á leitura e armazenamento numa matriz de dimensão 24*4 dos valores das
+           pulsações dos 4 pacientes ao longo das 24 horas de um dia;
+
+        b) Calcular e apresentar a média das pulsações para cada um dos pacientes;
+
+        c) Identificar a cama onde se encontra o paciente que apresentou o maior valor médio das
+           pulsações;
+
+        d) Identificar o valor mais elevado armazenado na matriz, e a identificação da cama e da hora
+           em que ocorreu.
+        */
+
         static void Main(string[] args)
         {
-            int[,] Infermaria = new int[24, 4];
+            
+            int[,] infermaria = new int[4, 24];
 
-            for (int i = 0; i < 24; i++)
+            for (int i = 0; i < 4; i++) // camas
             {
-                for (int j = 0; j < 4; j++)
-                
+                for (int j = 0; j < 24; j++) // horarios
+                {
+                    do
+                    {
+
+                        Console.Write($"Ensira a pusação do paciencia cama {i + 1} no horario {j + 1} : ");
+
+                    } while (!int.TryParse(Console.ReadLine(), out infermaria[i, j]));
+                }
+
             }
 
+
+            for (int i = 0; i < 4; i++) 
+            {
+                Console.Write($"Cama {i + 1}: ");
+                for (int j = 0; j < 24; j++)
+                {
+                    Console.Write(infermaria[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para ver a média das pulsações...");
+            Console.ReadKey();
+            Console.Clear();
+
+
+
+
+            int soma = 0;
+            double[] media = new double[4];
+                
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 0; j < 24; j++)
+                {
+                    soma += infermaria[i, j];
+                }
+                media[i] = soma / 24.0;
+                Console.WriteLine($"Cama {i + 1} - Media: {Math.Round(media[i],2)}");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para ver a  o maior valor médio das o maior valor médio das pulsações.");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            double maiorMadia = media[0];
+            int camaMaior = 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (media[i] > maiorMadia)
+                {
+                    maiorMadia = media[i];
+                    camaMaior++;
+                }
+               
+            }
+                Console.WriteLine($"Cama com maior média: {Math.Round(maiorMadia, 2)}");
+
             
-            
+            Console.WriteLine("Pressione qualquer tecla para ver a a identificação da cama e da hora em que ocorreu....");
+            Console.ReadKey();
+            Console.Clear();
 
+            int maior = infermaria[0, 0];
+            int horaMaior = 1;
 
+            for (int i = 0; i < 4; i++) 
+            {
+                for (int j = 0;j < 24; j++)
+                {
+                    if (infermaria[i, j] >maior)
+                    {
+                        maior = infermaria[i, j];
+                        camaMaior = i + 1;
+                        horaMaior = j + 1;
+                    }
+                }
+            }
 
-
-
-
-
-
-
-
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    for(int j = 0; j < 4; j++)
-            //    {
-            //        Console.WriteLine($"Pos[{i},{j}] = {tabela[i,j]}");
-            //    }
-            //}
+            Console.WriteLine($"Maior pulsação registrada: {maior} Cama: {camaMaior} Hora: {horaMaior}");
 
         }
     }
+
 }
