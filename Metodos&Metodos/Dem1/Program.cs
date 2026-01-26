f47b9abea1574f81c004b6260c18ea0215ba1739
p97arruda <p97arruda@gmail.com>
2026-01-26 20:18:01 +0000

    Metodos

---
+++ Metodos&Metodos/Dem1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+
+namespace Dem1
+{
+    internal class Program
+    {
+        //Elabore um procedimento que mostre as raízes quadradas dos inteiros de um dado intervalo.
+        //O programa principal deverá invocar este procedimento para mostrar as raízes quadradas dos
+        //números inteiros compreendidos no intervalo dado pelo utilizador.
+
+        // Parâmetro           Tipo      Significado
+        // Inf                 int       Limite   inferior do intervalo
+        // Sup                 int       Limite   superior do intervalo
+
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            //n1 = PedirNumero();
+            //n2 = PedirNumero();
+
+            
+        }
+
+        static void PedirNumero()
+        {
+            int numero;
+
+            do
+            {
+
+                Console.Write("Insira o numero: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out numero));
+        }
+
+
+        static void TrocaValor;
+
+
+
+
+        static void  MostrarRaizes(int Int, int Sup)
+        {
+            
+
+        }
+    }
+
+
+}
---
+++ Metodos&Metodos/Dem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dem2
+{
+    internal class Program
+    {
+
+        //2. Modifique o procedimento anterior, transformando-o numa função.
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"");
+
+        }
+
+        static void PedirNumero()
+        {
+            int numero;
+
+            do
+            {
+
+                Console.Write("Insira o numero: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out numero));
+        }
+
+        static int MostrarRaizes(int Int, int Sup)
+        {
+            return Math.Sqrt();
+        }
+
+    }
+}
