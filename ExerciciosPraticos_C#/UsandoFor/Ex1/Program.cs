81af09d5c7e8dada8ba5d9fc02d719bc4fd158ea
p97arruda <p97arruda@gmail.com>
2026-02-04 21:07:34 +0000

    Estudos logica

---
+++ ExerciciosPraticos_C#/UsandoFor/Ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 100;
+
+            //Console.Write("Insira um numero: ");
+            //int.TryParse(Console.ReadLine(), out numero);
+
+
+            for (int i = 0; i < numero; i++) 
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ ExerciciosPraticos_C#/UsandoFor/Ex_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero;
+            int numero, resultado; /*N = 1;*/
+
+             Console.Write("Insira um numero:");
+             int.TryParse(Console.ReadLine(), out numero);
+
+              for (int i = 1; i <= 10; i++) 
+              { 
+
+                  resultado = numero * i;
+                 Console.WriteLine($"{numero} x {i} = {resultado}");
+                 
+
+               }
+
+
+
+            //int numero, resultado, N = 1;
+
+            //Console.Write("Insira um numero entre 1 e 10: ");
+            //int.TryParse(Console.ReadLine(), out numero);
+
+            //while (N <= 10)
+            //{
+            //    resultado = numero * N;
+            //    Console.WriteLine($"{numero} * {N} = {resultado}");
+            //    N++;
+            //}
+        }
+    }
+}
