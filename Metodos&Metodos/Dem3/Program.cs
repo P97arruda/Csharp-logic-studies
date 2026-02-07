8c6f26535576413f35191b7aee59f2703bf2cf56
p97arruda <p97arruda@gmail.com>
2026-02-07 21:57:52 +0000

    Exercicios

--- Metodos&Metodos/Dem3/Program.cs
+++ Metodos&Metodos/Dem3/Program.cs
@@ -1,12 +1,53 @@
-﻿namespace Dem3
+﻿using System.Threading.Channels;
+
+namespace Dem3
 {
     internal class Program
     {
         // Elabore um procedimento que escreva os primeiros X nomes de um array.O procedimento tem
         // como parâmetros de entrada a lista de nomes e o número de elementos a selecionar.
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] nomes = new string[5];
+           int X = 0;
+
+            PreencherTabela(nomes);
+
+            MostarNomes(X);
+
+           // Console.WriteLine($"{nomes[X]}");
+
         }
+
+        static void PreencherTabela(string[] nomes)
+        {
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write($"Insira o nome {i + 1}: ");
+                nomes[i] = Console.ReadLine();
+            }
+        }
+
+        static void MostarNomes(int X) 
+        {
+            //int X;
+            
+            do
+            {
+
+                Console.WriteLine("Indique até que posição da tabela pretende visualizar os nomes:");
+
+            } while (!int.TryParse(Console.ReadLine(), out X));
+
+
+            for (int i = 0; i < X; i++) 
+            {
+                Console.WriteLine($"{X}");
+            }
+        
+        }
+
+    
+
     }
 }
