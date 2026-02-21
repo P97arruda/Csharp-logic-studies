79c1eba82a09ecda6b530adfb6355d763dc1c749
p97arruda <p97arruda@gmail.com>
2026-02-21 20:48:58 +0000

    Exercicios

---
+++ ExerciciosPraticos_C#/DesafiosPraticos/ExercicioFixacao/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace ExercicioFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritorio";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"Mesa de escritorio, cujo preço é $ {preco2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:f8} ");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:f3}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida:f3}" );
+        }
+    }
+}
