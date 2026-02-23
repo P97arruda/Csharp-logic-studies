f4f46d6ec427797f9d08333caafdef838f4be6ed
p97arruda <p97arruda@gmail.com>
2026-02-23 21:51:15 +0000

    ...

--- ExerciciosPraticos_C#/DesafiosPraticos/ValoresduplaPrecisão/Program.cs
+++ ExerciciosPraticos_C#/DesafiosPraticos/ValoresduplaPrecisão/Program.cs
@@ -4,7 +4,7 @@
     {
         // Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C.Em seguida, calcule e
         // mostre: 
-        // a) a área do triângulo retângulo que tem A por base e C por altura.
+        // a) a área do triângulo retângulo que tem A por base e C por altura
         // b) a área do círculo de raio C. (pi = 3.14159)
         // c) a área do trapézio que tem A e B por bases e C por altura.
         // d) a área do quadrado que tem lado B.
