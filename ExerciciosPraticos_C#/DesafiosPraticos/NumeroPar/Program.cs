9f2697a3bf73ce06c82af8e62b1f42a5076d3e6c
p97arruda <p97arruda@gmail.com>
2026-02-24 21:44:12 +0000

    Exercicios de logica

---
+++ ExerciciosPraticos_C#/DesafiosPraticos/NumeroPar/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NumeroPar
+{
+    internal class Program
+    {
+        // Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Insira um numero: ");
+            int.TryParse(Console.ReadLine(), out numero);
+
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+        }
+    }
+}
---
+++ ExerciciosPraticos_C#/DesafiosPraticos/NumeroPositivo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace NumeroPositivo
+{
+    internal class Program
+    {
+        // Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Insira um numero: ");
+            int.TryParse(Console.ReadLine(), out numero);
+
+            if (numero >= 0) 
+            {
+                Console.WriteLine("NÃO É NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+
+        }
+    }
+}
