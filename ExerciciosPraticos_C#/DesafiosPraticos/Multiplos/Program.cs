734dd92e9e9696161d7df79b6826195da393dca0
p97arruda <p97arruda@gmail.com>
2026-02-26 21:45:55 +0000

    Finalizando exercicios de logica

--- ExerciciosPraticos_C#/DesafiosPraticos/Multiplos/Program.cs
+++ ExerciciosPraticos_C#/DesafiosPraticos/Multiplos/Program.cs
@@ -7,7 +7,7 @@ namespace Multiplos
     {
         // Leia 2 valores inteiros(A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
         // Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
-        // ordem crescente ou decrescente
+        // ordem crescente ou decrescente.
         static void Main(string[] args)
         {
             int numero1, numero2;
