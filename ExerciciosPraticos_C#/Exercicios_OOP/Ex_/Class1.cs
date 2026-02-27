0ac2cb1c2aeabcc91d987414118e99cde7b4dc80
p97arruda <p97arruda@gmail.com>
2026-02-27 21:28:22 +0000

    OOP

---
+++ ExerciciosPraticos_C#/Exercicios_OOP/Ex_/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_
+{
+    internal class Class1
+    {
+        public string Nome;
+        public int Idade;
+
+    }
+}
---
+++ ExerciciosPraticos_C#/Exercicios_OOP/Ex_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Ex_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 n1, id1;
+
+            n1 = new Class1();
+            id1 = new Class1();
+
+
+
+            Console.WriteLine("Dados da primeira pessoa");
+
+            n1.Nome = Console.ReadLine();
+            id1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o nome da segunda pessoa");
+
+            n.Nome = Console.ReadLine();
+
+        }
+    }
+}
