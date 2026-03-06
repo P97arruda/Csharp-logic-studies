6209a8d52e850278d57582ca109b2826bdd8862b
p97arruda <p97arruda@gmail.com>
2026-03-06 21:48:51 +0000

    Exercicio OOP

--- ExerciciosPraticos_C#/Exercicios_OOP/Ex_3/Program.cs
+++ ExerciciosPraticos_C#/Exercicios_OOP/Ex_3/Program.cs
@@ -8,7 +8,7 @@ namespace Ex_3
         // Fazer um programa para ler os valores da largura e altura
         // de um retângulo.Em seguida, mostrar na tela o valor de
         // sua área, perímetro e diagonal.Usar uma classe como
-        // mostrado no projeto ao lado.
+        // mostrado no projeto ao lado
 
         static void Main(string[] args)
         {
